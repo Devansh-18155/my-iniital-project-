031e0ea15afc8dc226fb882864e52966026ee7de
Devansh-18155 <devansh.24bce10739@vitbhopal.ac.in>
2026-02-05 16:34:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button
+const button = document.getElementById("submitBtn");
+
+// Add click event
+button.addEventListener("click", function () {
+  alert("Button clicked! 🚀 JavaScript is working.");
+});
